2aec91b027b8a714a57ab0f707c671b608b6d3fb
JosAikema <jos@nextsense.nl>
2023-12-21 06:41:10 +0100

    Day 17 - WIP

---
+++ 17/day17.js
@@ -0,0 +1,27 @@
+class Challenge {
+    test_part1 = 102
+    test_part2 = 145
+    noSplit = false
+
+
+    solve_part1 (input) {
+
+        let sum = 0
+        let city = input.map((line) => line.split('').map(Number));
+        console.log(city)
+
+        queue.
+        return sum
+    }
+
+
+    solve_part2 (input) {
+
+        let sum = 0
+
+
+        return sum
+    }
+}
+
+module.exports = new Challenge();
